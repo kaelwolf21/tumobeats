5eb49a2431e19974ed8bd42801b606ea457ecd35
kaelwolf21 <lourenco.lobo@tumo.world>
2025-07-17 12:29:24 +0100

    Aula4

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ window.onload = async function() {
     let request= await fetch("data.json");
     let audioData = await request.json();
 
+    //carregar o service worker
+    if ("serviceWorker" in navigator) {
+        navigator.serviceWorker.register("service-worker.js")
+    }
+
     // Variaveis de elementos
     let title = document.querySelector("#title");
     let previousButton = document.querySelector("#previous-button");
@@ -30,6 +35,12 @@ window.onload = async function() {
     }
 
     previousButton.onclick = function() {
+        currentTrack--;
+        if(currentTrack < 0) {
+            currentTrack = audioData.length - 1;
+        }
+        playAudio();
+        
         console.log("previous button clicked");
     }
     playbutton.onclick = function() {
@@ -46,16 +57,32 @@ window.onload = async function() {
 
     scrubinput.querySelector("input").oninput = function(event) {
         let bar = scrubinput.querySelector(".range-bar");
-        updateInputBar(event.target.value, bar);
+        let value = event.target.value;
+        scrubaudio(value);
+        updateInputBar(value, bar);
     } 
     volumeinput.querySelector("input").oninput = function(event) {
         let bar = volumeinput.querySelector(".range-bar");
+        let value = event.target.value;
+        audio.volume = value / 100;
         updateInputBar(event.target.value, bar);
     } 
     fileinput.oninput = function(event) {
-    console.log("aqui")
+        let file = Array.from(fileInput.files)[0];
+        let reader = new FileReader();
+        reader.onload = function() {
+        audioData.push({
+        title: file.name,
+        url: reader.result
+        });
+        }
+        if (file) {
+        reader.readAsDataURL(file);
+        }
+        }
+        
        
-    }
+
     audio.onplay = function() {
         let playicon = document.querySelector("#icon-play");
         let pauseicon    = document.querySelector("#icon-pause");
@@ -68,14 +95,20 @@ window.onload = async function() {
         playicon.style.display = "block";
         pauseicon.style.display = "none";
 
-        console.log("audio esta pausado");
     }
+    audio.ontimeupdate = function() {
+        let value = (audio.currentTime / audio.duration) * 100;
+        let bar = scrubinput.querySelector(".range-bar");
+        updateInputBar(value, bar);
+    }
+
 
     function playAudio() {
         audio.src = audioData[currentTrack].url;
+        changeTitle(audioData[currentTrack].title);
         audio.play();
     }
-
+         
     function pauseAudio() {
         audio.pause();
 
@@ -83,4 +116,8 @@ window.onload = async function() {
     audioData.onplay = function() {
         console.log("audio esta a tocar");
     }
-}
+
+    function scrubaudio(value) {
+        audio.currentTime = (value / 100) * audio.duration;
+    }
+}     
